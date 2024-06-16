b7527c55d04a09016c663adb2be1edab6ed63017
Seav <Mrrseyla.758@gmail.com>
2024-06-16 18:24:43 +0700

    file : add comment to header component

--- components/header.js
+++ components/header.js
@@ -1,3 +1,4 @@
+//function to create header
 const createHeader = ()=>{
     return `
     <header class="text-header container border">
@@ -5,12 +6,17 @@ const createHeader = ()=>{
     </header>
     `
 }
+
+//Class for custom HTML element for the header component.
 class Header extends HTMLElement{
     constructor(){
         super()
     }
     connectedCallback(){
+        // Set the inner HTML of the element to the header
         this.innerHTML = createHeader()
     }
 }
+// Define a custom HTML element for the header component
 customElements.define("header-component",Header)
+
