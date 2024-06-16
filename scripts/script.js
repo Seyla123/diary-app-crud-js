2e655a55dcaaccf28397b3ec05261d0c80b53ba2
Seav <Mrrseyla.758@gmail.com>
2024-06-16 15:57:10 +0700

    fix : fixed onclick function in html tag using windown.function to it global

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 import { createCard } from "../components/card.js";
 
+// function for update data entry
 const updateEntry = (id) => {
     console.log('update', id);
 }
 
+// function for delete data entry
 const deleteEntry = (id) => {
     console.log('delete', id);
 }
@@ -43,16 +45,17 @@ const arr = [{
     content: 'lorem ipsum dolor sit amet',
     date: '1/1/2021',
 },]
-
 const containerListCard = document.querySelector('#list-card');
+// function for display data entry
 const displayEntry = ()=>{
-    
-
     let html = '';
-
     arr.map((item)=>{
         return html += createCard(item.id, item.title, item.content, item.date);
     })
     containerListCard.innerHTML = html;
 }
 displayEntry()
+
+
+window.updateEntry = updateEntry;
+window.deleteEntry = deleteEntry;
