c21dbd7759bc44db20e45f858c4053d9453ceb5f
Seav <Mrrseyla.758@gmail.com>
2024-06-16 14:24:11 +0700

    feature : create card entry component in card.js file

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const updateEntry = (id) => {
+    console.log('update', id);
+}
+
+const deleteEntry = (id) => {
+    console.log('delete', id);
+}
+const arr = [{
+    id: 1,
+    title: 'sometjing',
+    content: 'lorem ipsum dolor sit amet',
+    date: '1/1/2021',
+},
+{
+    id: 2,
+    title: 'sometjing',
+    content: 'lorem ipsum dolor sit amet',
+    date: '1/1/2021',
+},
+{
+    id: 3,
+    title: 'sometjing',
+    content: 'lorem ipsum dolor sit amet',
+    date: '1/1/2021',
+}]
+const containerListCard = document.querySelector('#list-card');
