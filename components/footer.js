19d64415164a18a630da1300b43d20208871ae18
Seav <Mrrseyla.758@gmail.com>
2024-06-23 20:16:53 +0700

    feature : add design to footer

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,30 @@
 //function to create footer
 const createFooter = ()=>{
     return `
-    <footer>Seav Seyla</footer>
+    <div id="viewModalSelection">
+    </div>
+    <div id="modal">
+      <delete-modal></delete-modal>
+      <update-modal></update-modal>
+    </div>
+    <footer>
+        
+        <div class="footer-left-container">
+            <h1 class="footer-title">SEAV SEYLA</h1>
+            <ul>
+                <li>Developed by : Seav Seyla</li>
+                <li>Email : Mrrseyla.758@gmail.com</li>
+                <li>Student ID : AB202410</li>
+                <li>Batch : 02</li>
+            </ul>
+        </div>
+        <span>|</span>
+        <div class="footer-right-container">
+            <h1 class="footer-title">Want to contact me ?</h1>
+            <input class="border input" type="text" placeholder="message here"></input>
+            <my-button title="Send Email"></my-button>
+        </div>
+    </footer>
     `
 }
 //Class for custom HTML element for the footer component.
