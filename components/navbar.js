dfdb7f3dfa4e08d5790fcd6a51e4ad6b6e4c913b
Seav <mrrseyla.758@gmail.com>
2024-07-02 18:15:32 +0700

    feature : add navbar component

---
+++ components/navbar.js
@@ -0,0 +1,29 @@
+const createNavbar = () => {
+    return `
+    <!-- navbar -->
+     <nav>
+      <div class="container-nav border">
+        <button id="logout-btn">
+          <img src="./assets/logout.png" alt="">
+          <span>logout</span>
+        </button>
+        <div class="profile-container">
+          <p id="profile-username"></p>
+          <button>
+            <img src="./assets/pf.png" alt="profile" />
+          </button>
+        </div>
+      </div>
+     </nav>
+    `
+}
+
+class Navbar extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = createNavbar()
+    }
+}
+window.customElements.define('navbar-component', Navbar)
