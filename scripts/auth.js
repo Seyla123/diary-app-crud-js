9373604dbeb83a81cacd67cb6f010750b6184706
Seav <mrrseyla.758@gmail.com>
2024-07-02 16:10:54 +0700

    file : add comment to auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,4 +1,8 @@
 
+/**
+ * This function checks if the user is logged in.
+ * If the user is not logged in, it redirects them to the login page.
+ */
 function authorize(){
     const isLogined = JSON.parse(localStorage.getItem("isLogined")) ? true : false;
     console.log('auth : ', isLogined);
@@ -6,11 +10,22 @@ function authorize(){
     window.location.href = '../pages/login.html';
   }
 }
+
+/**
+ * This function sets the user as logged in.
+ * It calls the authorize function to check if the user is logged in.
+ */
 function setLoginStatus(){
     localStorage.setItem("isLogined", JSON.stringify([true]));
     authorize();
 }
+
+/**
+ * This function sets the user as logged out.
+ * It calls the authorize function to check if the user is logged in.
+ */
 function setLogoutStatus(){
     localStorage.removeItem('isLogined');
+    window.location.href = '../'
     authorize();
 }
