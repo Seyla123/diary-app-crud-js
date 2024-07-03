25362f07e81bf50c147a1c62ac12d46461f3d589
Seav <mrrseyla.758@gmail.com>
2024-07-03 09:29:59 +0700

    file : add recent entry

--- components/recentEntry.js
+++ components/recentEntry.js
@@ -1,6 +1,5 @@
 const createRecentEntry = (id,title,content,date) => {
     return `
-    <div class="container">
               <div class="entry-card border">
                 <div class="entry-card-content">
                   <h3 class="entry-card-title">${title}</h3>
@@ -8,7 +7,6 @@ const createRecentEntry = (id,title,content,date) => {
                   <p class="entry-card-text">${content}</p>
                 </div>
               </div>
-    </div>
     `
 }
 
---
+++ scripts/dashboard.js
@@ -0,0 +1,23 @@
+import { createRecentEntry } from "../components/recentEntry.js";
+import { authorize } from "./auth.js";
+authorize();
+
+// function for display data entry
+const recentEntry = () => {
+    const containerRecentEntry = document.querySelector(".recent-entries-container");
+    // Retrieve the array of entries from local storage, or create an empty array if no entries exist.
+    let entries = JSON.parse(localStorage.getItem("diaryEntries")) || [];
+    if(entries.length > 0){
+      entries.reverse();
+      containerRecentEntry.innerHTML = entries
+        .map((item) => {
+          // Create a card element for the current entry and append it to the HTML for the container of card elements.
+          return createRecentEntry(item.id, item.title, item.content, item.date);
+        })
+        .join("");
+        viewModalFunc();
+    }else{
+      containerRecentEntry.innerHTML=`<not-found-data></not-found-data>`;
+    }
+  };
+  recentEntry()
